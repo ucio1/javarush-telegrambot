9cd917a5f99b474785acbd1d7772d31425d80ce5
Tanya <dmitrieva.hk@mail.ru>
2025-01-25 10:18:00 +0700

    JRTB-0: added SpringBot skeleton to the project.

---
+++ src/test/java/com/github/ucio1/jrtb/JavarushTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ucio1.jrtb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavarushTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
